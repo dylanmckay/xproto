9d69841722c3802bfd9d5900d52d9508b5f8131c
Dylan McKay <me@dylanmckay.io>
2018-11-23 04:26:12 +1300

    Disable warnings and add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+//! Rust transpilation of the `xproto` library.
+//!
+//! This library exposes types and values required
+//! to communicate with, or as, an X11 server.
+//!
+//! The `xproto` library does not include any logic,
+//! only data structures and constants.
+
+// Allow C style.
+#![allow(non_snake_case)]
+#![allow(non_camel_case_types)]
+#![allow(non_upper_case_globals)]
+
 mod bindings;
 
 pub use self::bindings::*;
